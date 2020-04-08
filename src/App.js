d9144439697c1376de3561e9ded2cfc8237545ca
Ersoy Efe Uruk <e.uruk@emakina.com.tr>
2020-04-08 21:03:32 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Total Cases Around the World</h1>
+      <div className="card-container">
+        <Card country="Turkey" short="TR"
+        api="https://api.apify.com/v2/key-value-stores/28ljlt47S5XEd1qIi/records/LATEST" />
+        <Card country="United Kingdom" short="UK"
+        api="https://api.apify.com/v2/key-value-stores/KWLojgM5r1JmMW4b4/records/LATEST" />
+        <Card country="Belgium" short="BE"
+        api="https://api.apify.com/v2/key-value-stores/apVM8aZ8hKZFvnKm7/records/LATEST" />
+        <Card country="Italy" short="IT" 
+        api="https://api.apify.com/v2/key-value-stores/UFpnR8mukiu0TSrb4/records/LATEST" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+import loading from '../../../src/assets/loading.gif';
+
+// Turkey, UK, Belgium, USA
+
+export default function Card(props) {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [tested, setTested] = useState(0);
+    const [infected, setInfected] = useState(0);
+    const [deceased, setDeceased] = useState(0);
+    const [recovered, setRecovered] = useState(0);
+
+
+    useEffect(() => {
+        fetch(props.api)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                setIsLoaded(true);
+                setTested(result?.tested);
+                setInfected(result?.infected);
+                setDeceased(result?.deceased);
+                setRecovered(result?.recovered);
+                if (props.country==="Belgium") {
+                    setDeceased(result?.deaths);
+                }
+                else if (props.country==="Italy") {
+                    setTested("N/A")
+                    setInfected(result?.totalCases);
+                    setRecovered(result?.dischargedHealed);
+                    
+                }
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }
+    else if (!isLoaded) {
+        return <img src={loading} width="10%" height="40%" />
+    }
+    else {
+        return (
+            <div className="card">
+                <h2 className={props.short}>{props.country}</h2>
+                <p>Total tested: {tested}</p>
+                <p>Total infected: {infected}</p>
+                <p style={{color: '#f23030'}}>Total deceased: {deceased}</p>
+                <p style={{color: '#00c91e'}}>Total recovered: {recovered}</p>
+            </div>
+        )
+    }
+}
