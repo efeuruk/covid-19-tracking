a8ea170d8cd80629b13def042c0f521548023ebe
Ersoy Efe Uruk <e.uruk@emakina.com.tr>
2020-04-08 21:08:45 +0300

    Updates

---
+++ precache-manifest.ada9816f7783b65d99753cb41de97f20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a7efb53fde0e2cd9157374e113e05e",
+    "url": "/efeuruk/covid-19-tracking/index.html"
+  },
+  {
+    "revision": "8bf368cec9f73e74536e",
+    "url": "/efeuruk/covid-19-tracking/static/css/main.3b131f41.chunk.css"
+  },
+  {
+    "revision": "02d6e2e32d7b1feed403",
+    "url": "/efeuruk/covid-19-tracking/static/js/2.4ac8fa6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/efeuruk/covid-19-tracking/static/js/2.4ac8fa6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf368cec9f73e74536e",
+    "url": "/efeuruk/covid-19-tracking/static/js/main.6fd62ee8.chunk.js"
+  },
+  {
+    "revision": "b23853be570cb425168e",
+    "url": "/efeuruk/covid-19-tracking/static/js/runtime-main.6141c097.js"
+  },
+  {
+    "revision": "77e0faeadcf2e569acf63040aa3038bc",
+    "url": "/efeuruk/covid-19-tracking/static/media/loading.77e0faea.gif"
+  }
+]);
