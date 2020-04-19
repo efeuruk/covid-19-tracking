175c90a255c2f9ad15a9da0faddb33df65d6df94
Ersoy Efe Uruk <e.uruk@emakina.com.tr>
2020-04-19 15:40:31 +0300

    N/A Warning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
     const date = new Date();
     return (
         <div>
+            <strong>*N/A's are existing because of the api source that I am using.</strong>
             <p>Copyright &copy; {date.getFullYear()} Ersoy Efe Uruk</p>
         </div>
     )
